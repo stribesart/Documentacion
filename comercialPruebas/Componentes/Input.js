311cf8d3d7c271ee666d74bbbaf59c9ea6f46214
stribesart <set_abner@hotmail.com>
2021-04-28 10:33:10 -0500

    input

--- comercialPruebas/Componentes/Input.js
+++ comercialPruebas/Componentes/Input.js
@@ -1,3 +1,16 @@
+/**
+ * Esta es una funcion que crea un elemento en el DOM de tipo Input
+ * @param {string} ANombreLabel - Parametro que define el texto que ira en el input
+ * @param {string} AType - Parametro que define el tipo de input que se va a realizar
+ * @param {string} AId - Parametro que define el identificador del elemento a insertar en el dom
+ * @param {string} AName - Parametro que define el nombre del elemento del dom
+ * @param {string} AValue - Parametro que define si se debe de insertar un valor al elemento, es opcional
+ * @param {int} ADisabled - Parametro que define si el elemento va a ir habilitado o deshabilitado
+ * @param {int} AChecked - Parametro que define si se le va a insertar un checkbox
+ * @returns DOMString
+ * @example
+ * Input(nombreLabel1, "radio", `radio-${nombreLabel1}`, "tipoVenta", "0", 1);
+ */
 export function Input(ANombreLabel,AType,  AId, AName, AValue = '', ADisabled = 1, AChecked = 1){
     const $container = document.createElement('div')
     const $label = document.createElement('label');
